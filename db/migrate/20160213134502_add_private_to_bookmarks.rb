98466973c510e12dabe8e61e3109fd0a26f33107
Stephen McCullough <steve@shopkeep.com>
2016-02-13 14:09:19 +0000

    [#2] - Model can now have a private column

---
+++ db/migrate/20160213134502_add_private_to_bookmarks.rb
@@ -0,0 +1,5 @@
+class AddPrivateToBookmarks < ActiveRecord::Migration
+  def change
+    add_column :bookmarks, :private, :boolean, default: false
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160209131729) do
+ActiveRecord::Schema.define(version: 20160213134502) do
 
   create_table "bookmarks", force: :cascade do |t|
     t.string   "title"
     t.string   "url"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
+    t.boolean  "private",    default: false
   end
 
 end
